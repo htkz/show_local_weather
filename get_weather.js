a2f3641707981d4bfe731d8481eb02cbe9eac5c7
htkz <htkzmo@gmail.com>
2017-09-18 17:28:58 -0400

    Finished

--- get_weather.js
+++ get_weather.js
@@ -1,31 +1,44 @@
-$(document).ready(function() {
-  $.simpleWeather({
-    location: 'Austin, TX',
-    woeid: '',
-    unit: 'f',
-    success: function(weather) {
-      html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
-      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
-      html += '<li class="currently">'+weather.currently+'</li>';
-      html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
-
-      var temp_s = Math.floor((parseInt(weather.temp) - 32) / 1.8);
-      document.getElementById("temp-f").innerHTML = weather.temp+'&deg'+ '<span id="change-temp-f">F<span>';
-      document.getElementById("temp-s").innerHTML = temp_s.toString()+'&deg'+ '<span id="change-temp-s">C<span>';
-
-      $("#weather").html(html);
-    },
-    error: function(error) {
-    //   $("#weather").html('<p>'+error+'</p>');
+var temp_c, temp_f;
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showWeather);
+    } else {
+        console.log("Geolocation is not supported by this browser.");
     }
-  });
-});
+}
 
-$("#change-temp-f").onclik(function() {
-    $("#temp-f").style.display = "inline";
-    $("#temp-s").style.display = "none";
-})
-$("#change-temp-s").onclik(function() {
-    $("#temp-s").style.display = "inline";
-    $("#temp-f").style.display = "none";
-})
+function showWeather(position) {
+    var latitude =  position.coords.latitude;
+    var longitude = position.coords.longitude;
+    var url = "https://fcc-weather-api.glitch.me/api/current?"+"lat="+latitude +"&"+"lon="+longitude;
+    $.get(url,function(weather){
+        console.log(weather);
+        temp_c = Math.floor(weather.main.temp);
+        temp_f = Math.floor((parseInt(temp_c * 1.8 + 32)));
+        var city = weather.name;
+        var country = weather.sys.country;
+        var weather_info = weather.weather[0];
+        var desc = weather_info.description;
+        var desc_cap = desc.charAt(0).toUpperCase() + desc.slice(1)
+        var icon = weather_info.icon;
+        $("#icon").html("<img src="+icon+" />");
+        $("#city").html(city+', ' + country);
+        $("#desc").html(desc_cap);
+        $("#temp").html(temp_c+"&degC")
+    })
+}
+
+changeType = function() {
+    if($("#temp").val() === "c") {
+        $("#temp").html(temp_f+"&degF")
+        $("#temp").val("f");
+    } else {
+        $("#temp").html(temp_c+"&degC")
+        $("#temp").val("c");
+    }
+}
+
+
+
+getLocation();
+$("#change-type").click(changeType);
